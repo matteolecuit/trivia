77a6708b35395d1acd3a7b4a25628e66eb48578a
Paul <paulereverend@gmail.com>
2022-03-10 14:17:33 +0100

    add gold parameter

--- typescript/src/game.ts
+++ typescript/src/game.ts
@@ -2,22 +2,23 @@ import { askRockType, initPlayers } from "./utils";
 import { Player, Questions } from "./types";
 
 export class Game {
-  public players: Player[];
-  public currentPlayer: number = 0;
-  public isRock: boolean;
+    public players: Player[];
+    public currentPlayer: number = 0;
+    public isRock: boolean;
+    public maxGold: number;
+    public questions: Questions;
 
-  public questions: Questions;
-
-  constructor(playerNames: string[]) {
-    this.players = initPlayers(playerNames);
-    this.questions = { pop: [], science: [], rock: [], sports: [], techno: [] };
-    this.isRock = askRockType();
-    for (let i = 0; i < 50; i++) {
-      this.questions.pop.push("Pop Question " + i);
-      this.questions.science.push("Science Question " + i);
-      this.questions.sports.push("Sports Question " + i);
-      if (this.isRock) this.questions.rock.push("Rock Question " + i);
-      else this.questions.techno.push("Rock Question " + i);
+    constructor(playerNames: string[], maxGold: number) {
+        this.players = initPlayers(playerNames);
+        this.maxGold = maxGold;
+        this.questions = { pop: [], science: [], rock: [], sports: [], techno: [] };
+        this.isRock = askRockType();
+        for (let i = 0; i < 50; i++) {
+            this.questions.pop.push("Pop Question " + i);
+            this.questions.science.push("Science Question " + i);
+            this.questions.sports.push("Sports Question " + i);
+            if (this.isRock) this.questions.rock.push("Rock Question " + i);
+            else this.questions.techno.push("Rock Question " + i);
+        }
     }
-  }
 }
