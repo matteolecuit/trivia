c0a3d0013da616ac26aa6b2c425bfef401b92853
Matteo Lecuit <matteo@lecuit.fr>
2022-03-10 09:26:27 +0100

    start game with player names

--- typescript/src/game-runner.ts
+++ typescript/src/game-runner.ts
@@ -1,27 +1,23 @@
-import {Game} from './game';
+import { Game } from "./game";
 
 export class GameRunner {
-    public static main(): void {
-        const game = new Game();
-        game.add("Chet");
-        game.add("Pat");
-        game.add("Sue");
+  public static main(): void {
+    const game = new Game(["Chet", "Pat", "Sue"]);
+    game.add("Chet");
+    game.add("Pat");
+    game.add("Sue");
 
-        let notAWinner;
-        do {
+    let notAWinner;
+    do {
+      game.roll(Math.floor(Math.random() * 6) + 1);
 
-            game.roll(Math.floor(Math.random() * 6) + 1);
-        
-            if (Math.floor(Math.random() * 10) == 7) {
-            notAWinner = game.wrongAnswer();
-            } else {
-            notAWinner = game.wasCorrectlyAnswered();
-            }
-        
-        } while (notAWinner);
-    }
+      if (Math.floor(Math.random() * 10) == 7) {
+        notAWinner = game.wrongAnswer();
+      } else {
+        notAWinner = game.wasCorrectlyAnswered();
+      }
+    } while (notAWinner);
+  }
 }
 
 GameRunner.main();
-
-  
