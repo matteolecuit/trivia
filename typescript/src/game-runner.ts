3d6dc7aeb0a188caca304cd9ae1f8fecef9f0c63
Paul <paulereverend@gmail.com>
2022-03-10 14:37:07 +0100

    fix switch players

--- typescript/src/game-runner.ts
+++ typescript/src/game-runner.ts
@@ -1,43 +1,44 @@
 import { Game } from "./game";
-import { checkPlayers, roll, wasCorrectlyAnswered, wrongAnswer } from "./utils";
+import { checkPlayers, roll, switchPlayer, wasCorrectlyAnswered, wrongAnswer } from "./utils";
 
 export class GameRunner {
-  public static main(): void {
-    const game = new Game(["Chet", "Pat", "Sue"], 6);
+    public static main(): void {
+        const game = new Game(["Chet", "Pat", "Sue"], 6);
 
-    const isGameValid = checkPlayers(game.players);
-    if (!isGameValid) {
-      console.error("Game should have more than 2 and less than 7 players");
-      return;
-    }
-    let gameHasEnded = false;
-    let notAWinner = true;
-    do {
-      const diceRoll = Math.floor(Math.random() * 6) + 1;
-      let action = roll(
-        game.players,
-        game.currentPlayer,
-        game.questions,
-        game.isRock,
-        diceRoll
-      );
-
-      if (action != 2) {
-        if (Math.floor(Math.random() * 10) == 7) {
-          wrongAnswer(game.players, game.currentPlayer);
-        } else {
-          let winner = wasCorrectlyAnswered(
-            game.players,
-            game.currentPlayer,
-            game.maxGold
-          );
-          if (winner) gameHasEnded = true;
+        const isGameValid = checkPlayers(game.players);
+        if (!isGameValid) {
+            console.error("Game should have more than 2 and less than 7 players");
+            return;
         }
-      } else {
-        notAWinner = true;
-      }
-    } while (!gameHasEnded);
-  }
+        let gameHasEnded = false;
+        let notAWinner = true;
+        do {
+            const diceRoll = Math.floor(Math.random() * 6) + 1;
+            let action = roll(
+                game.players,
+                game.currentPlayer,
+                game.questions,
+                game.isRock,
+                diceRoll
+            );
+
+            if (action != 2) {
+                if (Math.floor(Math.random() * 10) == 7) {
+                    wrongAnswer(game.players, game.currentPlayer);
+                } else {
+                    let winner = wasCorrectlyAnswered(
+                        game.players,
+                        game.currentPlayer,
+                        game.maxGold
+                    );
+                    if (winner) gameHasEnded = true;
+                }
+            } else {
+                notAWinner = true;
+            }
+            game.currentPlayer = switchPlayer(game.currentPlayer, game.players);
+        } while (!gameHasEnded);
+    }
 }
 
 GameRunner.main();
