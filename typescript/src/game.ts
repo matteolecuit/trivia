ef4c70c80250ac9409bb54bee7b658bd209af222
Matteo Lecuit <matteo@lecuit.fr>
2022-03-11 08:58:01 +0100

    add auto mode

--- typescript/src/game.ts
+++ typescript/src/game.ts
@@ -1,31 +1,34 @@
-import { askRockType, generateQuestions, initPlayers } from "./utils";
+import { generateQuestions, initPlayers } from "./utils";
 import { Player, Questions } from "./types";
+import { getRockTypeFromPrompt } from "./prompt-service";
 
 export class Game {
-    public players: Player[];
-    public currentPlayer: number = 0;
-    public isRock: boolean;
-    public maxGold: number;
-    public questions: Questions;
-	public leaderboard: Player[];
-	public rageQuitBoard: Player[];
-    public nextCategory: string;
+  public players: Player[];
+  public currentPlayer: number = 0;
+  public isRock: boolean;
+  public maxGold: number;
+  public questions: Questions;
+  public leaderboard: Player[];
+  public rageQuitBoard: Player[];
+  public nextCategory: string;
+  public autoMode: boolean;
 
-    constructor(playerNames: string[], maxGold: number) {
-        this.players = initPlayers(playerNames);
-        this.maxGold = maxGold;
-        this.questions = { pop: [], science: [], rock: [], sports: [], techno: [] };
-        this.isRock = askRockType();
-		this.leaderboard = [];
-		this.rageQuitBoard = [];
-        this.nextCategory = "";
-        generateQuestions(this.questions, 2, this.isRock);
-        /*for (let i = 0; i < 50; i++) {
+  constructor(playerNames: string[], maxGold: number, autoMode: boolean) {
+    this.players = initPlayers(playerNames);
+    this.maxGold = maxGold;
+    this.questions = { pop: [], science: [], rock: [], sports: [], techno: [] };
+    this.isRock = getRockTypeFromPrompt(autoMode);
+    this.leaderboard = [];
+    this.rageQuitBoard = [];
+    this.nextCategory = "";
+    this.autoMode = autoMode;
+    generateQuestions(this.questions, 2, this.isRock);
+    /*for (let i = 0; i < 50; i++) {
             this.questions.pop.push("Pop Question " + i);
             this.questions.science.push("Science Question " + i);
             this.questions.sports.push("Sports Question " + i);
             if (this.isRock) this.questions.rock.push("Rock Question " + i);
             else this.questions.techno.push("Rock Question " + i);
         }*/
-    }
+  }
 }
--- typescript/src/utils.ts
+++ typescript/src/utils.ts
@@ -1,6 +1,5 @@
+import { getActionFromPrompt, getCategoryFromPrompt } from "./prompt-service";
 import { Category, Player, Questions } from "./types";
-import * as readline from "readline-sync";
-import { categories } from '../enums/categories.enum';
 
 export const initPlayers = (playerNames: string[]) => {
   let playerCount = 0;
@@ -27,14 +26,20 @@ export const didPlayerWin = (player: Player, maxGold: number) => {
   return player.gold >= maxGold;
 };
 
-export const currentCategory = (player: Player, isRock: boolean, nextCategory: string) => {
+export const currentCategory = (
+  player: Player,
+  isRock: boolean,
+  nextCategory: string
+) => {
   if (nextCategory.length > 0) {
     let choosenCategory = nextCategory;
-    console.log(`The category has be defined previously and will be ${choosenCategory}`);
+    console.log(
+      `The category has be defined previously and will be ${choosenCategory}`
+    );
     nextCategory = "";
     return choosenCategory;
   }
-  
+
   let category: Category = "rock";
   if (player.place == 0) category = "pop";
   if (player.place == 1) category = "science";
@@ -59,19 +64,23 @@ export const askQuestion = (
     const category = currentCategory(player, isRock, nextCategory);
     const availableQuestions = questions[category] as string[];
     if (availableQuestions.length <= 0) {
-      generateQuestions
-        (questions, 10, isRock);
+      generateQuestions(questions, 10, isRock);
     }
     console.log(availableQuestions.shift());
   }
 };
 
-export const wrongAnswer = (players: Player[], currentPlayer: number, nextCategory: string) => {
+export const wrongAnswer = (
+  players: Player[],
+  currentPlayer: number,
+  nextCategory: string,
+  autoMode: boolean
+) => {
   const player = players[currentPlayer];
   console.log("Question was incorrectly answered");
   console.log(player.name + " was sent to the penalty box");
   player.isInPenaltyBox = true;
-  let designedCategory = askCategory(player);
+  let designedCategory = getCategoryFromPrompt(player, autoMode);
   player.prison += 1;
 
   player.streak = 0;
@@ -81,7 +90,7 @@ export const wrongAnswer = (players: Player[], currentPlayer: number, nextCatego
   currentPlayer += 1;
   if (currentPlayer == players.length) currentPlayer = 0;
 
-  return designedCategory
+  return designedCategory;
 };
 
 export const switchPlayer = (currentPlayer: number, players: Player[]) => {
@@ -96,23 +105,23 @@ export const wasCorrectlyAnswered = (
 ) => {
   const player = players[currentPlayer];
   if (!player.hasQuit) {
-      console.log("Answer was correct!!!!");
+    console.log("Answer was correct!!!!");
 
-      player.streak += 1;
-      player.gold += player.streak;
+    player.streak += 1;
+    player.gold += player.streak;
 
-      console.log(
-        "🔥" +
+    console.log(
+      "🔥" +
         player.name +
         " now 2has " +
         player.gold +
         " Gold Coins and has a streak of " +
         player.streak
-      );
+    );
 
-      var winner = didPlayerWin(player, maxGold);
+    var winner = didPlayerWin(player, maxGold);
 
-      return winner;
+    return winner;
   } else {
     return false;
   }
@@ -123,72 +132,6 @@ export const checkPlayers = (players: Player[]) => {
     return true;
   }
   return false;
-};
-
-export const askRockType = () => {
-  let rockPrompt: string = readline.question(
-    "Tu veux du rock mon copain ? (Y/N) : "
-  );
-
-  if (rockPrompt.toLowerCase() === "y") {
-    console.log("You choose Rock");
-    return true;
-  } else if (rockPrompt.toLowerCase() === "n") {
-    console.log("You'll have Techno questions");
-    return false;
-  } else {
-    console.log("Invalid answer, You'll have Techno questions");
-    return false;
-  }
-};
-
-export const askAction = (player: Player, rageQuitBoard: Player[]) => {
-  let askPrompt: string = readline.question(
-    "Choisissez votre action ? : \
-    1- Répondre à la question \
-    2- Utiliser un joker \
-    3- Quitter la partie"
-  );
-
-  if (askPrompt === "1") {
-    console.log("You'll answer to this question");
-  } else if (askPrompt === "2") {
-    if (player.jokers > 0) {
-      console.log("You skip the question, you lost your Joker !");
-    } else {
-      console.log("You don't have any jokers, You'll answer to this question");
-      return 1;
-    }
-  } else if (askPrompt === "3") {
-    console.log("You are out !");
-    player.hasQuit = true;
-    rageQuitBoard.push(player);
-  } else {
-    console.log("Invalid answer, You'll answer to this question");
-    return 0;
-  }
-  return Number(askPrompt);
-};
-
-export const askCategory = (player: Player) => {
-  let isValid = false;
-  let askPrompt = "";
-  let validCategories: string[] = Object.values(categories);
-
-  do {
-    console.log(validCategories);
-    askPrompt = readline.question(
-      "Which category would you like to give to next player ? : "
-    );
-
-    if (validCategories.includes(askPrompt)) {
-      isValid = true;
-    } else {
-      console.log("Please choose a valid category")
-    }
-  } while (!isValid)
-
-  return askPrompt;
 };
 
 export const createRockQuestion = (index: number, isRock: boolean) => {
@@ -204,7 +147,8 @@ export const roll = (
   isRock: boolean,
   roll: number,
   nextCategory: string,
-  rageQuitBoard: Player[]
+  rageQuitBoard: Player[],
+  autoMode: boolean
 ) => {
   const player = players[currentPlayer];
   console.log(player.name + " is the current player");
@@ -212,11 +156,16 @@ export const roll = (
 
   if (!player.hasQuit) {
     if (player.isInPenaltyBox) {
-      const rollExitPrison = Math.floor(Math.random() * player.prison) + 1
-      console.log('🏃🏃You have 1/'+player.prison+" chance to exit. Result must be 1 : "+rollExitPrison);
+      const rollExitPrison = Math.floor(Math.random() * player.prison) + 1;
+      console.log(
+        "🏃🏃You have 1/" +
+          player.prison +
+          " chance to exit. Result must be 1 : " +
+          rollExitPrison
+      );
       if (rollExitPrison == 1) {
         player.isInPenaltyBox = false;
-        console.log("🏃 "+player.name + " is getting out of the penalty box");
+        console.log("🏃 " + player.name + " is getting out of the penalty box");
         player.place = move(player, roll);
       } else {
         console.log(player.name + " is not getting out of the penalty box");
@@ -226,8 +175,10 @@ export const roll = (
     player.place = move(player, roll);
 
     console.log(player.name + "'s new location is " + player.place);
-    console.log("The category is " + currentCategory(player, isRock, nextCategory));
-    if (askAction(player, rageQuitBoard) == 2) {
+    console.log(
+      "The category is " + currentCategory(player, isRock, nextCategory)
+    );
+    if (getActionFromPrompt(player, rageQuitBoard, autoMode) == 2) {
       player.jokers--;
       return 2;
     }
@@ -235,7 +186,6 @@ export const roll = (
     askQuestion(player, questions, isRock, nextCategory);
 
     return 0;
-
   }
 };
 
@@ -246,9 +196,13 @@ export const move = (player: Player, roll: number) => {
   }
   console.log(player.name + "'s new location is " + player.place);
   return player.place;
-}
+};
 
-export const generateQuestions = (questions: Questions, amount: number, isRock: boolean) => {
+export const generateQuestions = (
+  questions: Questions,
+  amount: number,
+  isRock: boolean
+) => {
   console.log("🃏 Generating " + amount + " cards...");
   for (let i = 0; i < amount; i++) {
     questions.pop.push("Pop Question " + i);
@@ -258,4 +212,4 @@ export const generateQuestions = (questions: Questions, amount: number, isRock: 
     else questions.techno.push("Rock Question " + i);
   }
   return questions;
-}
+};
