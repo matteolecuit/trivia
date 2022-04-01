db3f5cbb9fe36311a59257ba79be77cf85877277
Matteo Lecuit <matteo@lecuit.fr>
2022-04-01 08:28:20 +0200

    add consoleLog in utils

--- typescript/src/utils.ts
+++ typescript/src/utils.ts
@@ -1,3 +1,7 @@
 export const generateRandom = (limit: number, fixedNumber?: number) => {
   return fixedNumber ?? Math.floor(Math.random() * limit) + 1;
 };
+
+export const consoleLog = (message: string) => {
+  console.log(message);
+};
